073dc9469d352bbf267dfb9f856ac78985bb667c
NikiLis26 <120452720+NikiLis26@users.noreply.github.com>
2024-04-21 11:42:41 +0300

    New commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -10,40 +10,54 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Announcement(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    heading = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(300), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    owner = db.Column(db.String(100), nullable=False)
-
+   id = db.Column(db.Integer, primary_key=True)
+   heading = db.Column(db.String(100), nullable=False)
+   description = db.Column(db.String(300), nullable=False)
+   date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+   owner = db.Column(db.String(100), nullable=False)
 
 @app.route('/create', methods=['GET'])
 def create_form():
-
-    return render_template('create_form.html')
-
+   return render_template('create_form.html')
 
 @app.route('/create', methods=['POST'])
 def create_ad():
-
-    return redirect(url_for('index'))
-
+   data = request.form
+   new_ad = Announcement(heading=data['heading'], description=data['description'], owner=data['owner'])
+   try:
+       db.session.add(new_ad)
+       db.session.commit()
+   except:
+       db.session.rollback()
+   return redirect(url_for('index'))
 
 @app.route('/delete/<int:ad_id>', methods=['POST'])
 def delete_ad(ad_id):
-
-    return redirect(url_for('index'))
+   ad_to_delete = Announcement.query.get_or_404(ad_id)
+   try:
+       db.session.delete(ad_to_delete)
+       db.session.commit()
+   except:
+       db.session.rollback()
+   return redirect(url_for('index'))
 
 @app.route('/edit/<int:ad_id>', methods=['GET'])
 def edit_form(ad_id):
-    ad = Announcement.query.get_or_404(ad_id)
-    return render_template('edit_form.html', ad=ad)  # Передача данных объявления в шаблон
-
+   ad = Announcement.query.get_or_404(ad_id)
+   return render_template('edit_form.html', ad=ad)
 
 @app.route('/edit/<int:ad_id>', methods=['POST'])
 def edit_ad(ad_id):
-
-    return redirect(url_for('index'))  # Перенаправление на главную страницу после редактирования
+   ad_to_edit = Announcement.query.get_or_404(ad_id)
+   data = request.form
+   ad_to_edit.heading = data['heading']
+   ad_to_edit.description = data['description']
+   ad_to_edit.owner = data['owner']
+   try:
+       db.session.commit()
+   except:
+       db.session.rollback()
+   return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
